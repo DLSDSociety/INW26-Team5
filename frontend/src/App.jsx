1f32f2925e30e779286caf7f2687aa783e0b92d9
Mustafa-Azad18 <azad83801@gmail.com>
2026-02-16 22:43:41 +0530

    Frontend and backend initial testing version

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <h1>Job and Career Portal</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Job and Career Portal</h1>
+      <p>Find your dream job here.</p>
+    </div>
+  );
+}
+
+export default Home;
